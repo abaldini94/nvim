4edced8f2285eb0f06ae2f2f7229f745ef67a0b7
Andrea Baldini <baldiniandrea1994@gmail.com>
2025-01-07 14:27:51 +0100

    Refactored project structure

---
+++ lua/plugins.lua
@@ -0,0 +1,23 @@
+local plugins = {
+	"tpope/vim-sleuth",
+	require("plugins.lsp"),
+	require("plugins.gitsigns"),
+	require("plugins.whichkey"),
+	require("plugins.telescope"),
+	require("plugins.autoformat"),
+	require("plugins.autocompletion"),
+	require("plugins.colorscheme"),
+	require("plugins.todos"),
+	require("plugins.mini"),
+	require("plugins.treesitter"),
+	require("plugins.neotree"),
+	require("plugins.toggleterm"),
+}
+
+local opts = {
+	ui = {
+		icons = {},
+	},
+}
+
+require("lazy").setup(plugins, opts)
